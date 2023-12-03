ab71569d86fe1ed6da5540c2fb96af7025e9585a
Reed Anderson <reeda@ksu.edu>
2023-12-02 22:16:27 -0600

    transfer back

--- src/days/day_02_a.rs
+++ src/days/day_02_a.rs
@@ -26,8 +26,25 @@ fn id_of_line_if_possible(line: &str) -> Option<i32> {
 }
 
 fn is_game_possible(game_results_str: &str) -> bool {
-    println!("{}", game_results_str);
-    true
+    let invalid_game = game_results_str.split(";").find(|game| {
+        let results = game.split(",");
+        // let invalid_result = results.find(|result| )
+        return true;
+    });
+    return  invalid_game.is_none();
+}
+
+fn is_singe_result_possible(single_result: &str) -> bool {
+    let str_vec: Vec<&str> = single_result.split_whitespace().collect();
+    let str_num = str_vec[0];
+    let str_color = str_vec[1];
+
+    return match str_color {
+        "red" => str_num.parse::<i32>().unwrap() <= 12,
+        "blue" => str_num.parse::<i32>().unwrap() <= 12,
+        "green" => str_num.parse::<i32>().unwrap() <= 12,
+        _ => false
+    };
 }
 
 fn get_line_id(game_id_str: &str) -> i32 {
